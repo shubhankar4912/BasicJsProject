3e111d8426809b7064313167b312d8f9e847e583
shubhankar4912 <97121506+shubhankar4912@users.noreply.github.com>
2023-08-28 18:09:41 +0000

    Bmicalculator

---
+++ BMIcalculator/index.js
@@ -0,0 +1,29 @@
+const form=document.querySelector('form')
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+  const height=parseInt(document.querySelector('#height').value);
+  const weight=parseInt(document.querySelector('#weight').value);
+  const results=document.querySelector('#results');
+  if(height===''||height<0||isNaN(height)){
+    results.innerHTML=`Please enter valid height ${height}`
+  }
+  else if(weight===''||weight<0||isNaN(weight)){
+    results.innerHTML=`Please enter valid weight ${height}`
+  }
+  else{
+    const bmi=(weight / ((height * height) / 10000)).toFixed(2);
+    if(bmi<18.6){
+    results.innerHTML=`<span>Under weight <br>${bmi}</span>`
+    }
+    else if(18.6<bmi&&bmi<24.9){
+      results.innerHTML=`<span>Normal range <br>${bmi}</span>`
+    }
+    else{
+      results.innerHTML=`<span>Overweight <br>${bmi}</span>`
+    }
+    
+  }
+
+
+
+})
